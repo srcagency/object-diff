6f752f451cfa27d47975d1a32164e67a7e430704
Thomas Jensen <thomas@src.agency>
2015-02-05 22:56:32 +0100

    use es6 rest parameters

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 'use strict';
 
-export default function(){
-	let ref = arguments[0];
+export default function( ref, ...cs ){
 	let diff = {};
 
-	for (let c of arguments) {
+	for (let c of cs) {
 		let keys = Object.keys(c);
 
 		for (let key of keys) {
