6e5417df6e7425ee5716040baeec9ff2d93c9b86
tangxinfa <tangxinfa@gmail.com>
2016-04-24 18:15:31 +0800

    support "equal" option.
    use "===" by default.

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
 'use strict';
 
-module.exports = diff;
-
-function diff(){
-	var length = arguments.length;
-	var ref = arguments[0];
-	var diff = {};
-	var c;
-	var keys;
-	var keysLength;
-	var key;
-	var u;
-
-	for (var i = 1;i < length;i++) {
-		c = arguments[i];
-		keys = Object.keys(c);
-		keysLength = keys.length;
-
-		for (u = 0;u < keysLength;u++) {
-			key = keys[u];
-
-			if (c[key] !== ref[key])
-				diff[key] = c[key];
-		}
+var defaults = {
+	equal: function (a, b) {
+		return a === b;
 	}
+};
 
-	return diff;
+function create(options) {
+	var equal = options && options.equal || defaults.equal;
+
+	return function diff(){
+		var length = arguments.length;
+		var ref = arguments[0];
+		var diff = {};
+		var c;
+		var keys;
+		var keysLength;
+		var key;
+		var u;
+
+		for (var i = 1;i < length;i++) {
+			c = arguments[i];
+			keys = Object.keys(c);
+			keysLength = keys.length;
+
+			for (u = 0;u < keysLength;u++) {
+				key = keys[u];
+
+				if (! equal(c[key], ref[key]))
+					diff[key] = c[key];
+			}
+		}
+
+		return diff;
+	}
 }
+
+
+var diff = create(defaults);
+diff.create = create;
+
+module.exports = diff;
